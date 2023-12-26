d3ebcf7c54ea41ab4978964382e3064a191fd2c1
Ebhota Jonathan <joe.ebh100@live.com>
2023-12-26 23:56:42 +0100

    Feature: Product & Category

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import getBillboard from "@/actions/get-billboards";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/Billboard";
+import ProductList from "@/components/ProductList";
+import Container from "@/components/ui/container";
 
-const HomePage = () => {
-  return <div>HomePage</div>;
+export const revalidate = 0; //Prevent Caching
+
+const HomePage = async () => {
+  const products = await getProducts({
+    isFeatured: true,
+    categoryId: "",
+    colorId: "",
+    sizeId: "",
+  });
+  const billboard = await getBillboard("891c157f-833b-4edf-bb87-1525a070f129");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
