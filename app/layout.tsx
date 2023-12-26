a7079e3e8418d6d9f51be39a55af20f072b1f5db
Ebhota Jonathan <joe.ebh100@live.com>
2023-12-26 15:39:28 +0100

    Initial Setup

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Urbanist } from "next/font/google";
+import "./globals.css";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
+const urbanist = Urbanist({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Papi Commerce",
+  description: "Store Front",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={urbanist.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
